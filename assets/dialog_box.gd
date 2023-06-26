20fdb4dd59ac749ce8b0adf46bce2b1e72d9729e
MARCHAND MANON <manonmarchand22@gmail.com>
2023-06-26 11:52:33 +0200

    compile for web

--- assets/dialog_box.gd
+++ assets/dialog_box.gd
@@ -1,9 +1,11 @@
 extends ColorRect
 
 @export var dialogPath:String = "res://assets/dialog.json"
-@export var textSpeed:float = 0.05
+@export var textSpeed:float = 0.02
 
 var dialog
+
+
 
 var phrase_num:int = 0
 var finished:bool = false 
@@ -13,6 +15,7 @@ func _ready():
 	dialog = getDialog()
 	assert(dialog, "Dialog not found")
 	nextPhrase()
+	
 	
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
@@ -34,6 +37,7 @@ func nextPhrase() -> void:
 		
 	finished = false
 	$text.bbcode_text = dialog[phrase_num]["text"]
+	
 	$text.visible_characters = 0
 	while $text.visible_characters < len($text.text):
 		$text.visible_characters += 1
@@ -41,6 +45,34 @@ func nextPhrase() -> void:
 		await $"../Timer".timeout
 		
 	finished = true
+	var step: int = dialog[phrase_num]["step"]
+	print(step)
+	match step:
+		0: 
+			$"../../step1".hide()
+			$"../../step2".hide()
+			$"../../step3".hide()
+			$"../../step4".hide()
+			$"../../step5".hide()
+			$"../../logos/Ivoa".hide()
+			$"../../logos/B2Find".hide()
+			
+		1:
+			$"../../step1".show()
+		2:
+			$"../../step2".show()
+		3:
+			$"../../step3".show()
+		4: 
+			$"../../step4".show()
+		5: 
+			$"../../step5".show()
+		6: 
+			$"../../logos/Ivoa".show()
+		7: 
+			$"../../logos/B2Find".show()
+		
+
 	phrase_num += 1
 	return
 
